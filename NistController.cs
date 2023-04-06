26167f11735517f66e16eb626137fa6cfc9d5cdb
azay10 <117178019+azay10@users.noreply.github.com>
2023-04-06 08:49:40 +0545

    Add files via upload

---
+++ NistController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace myfirstWebApp.Controllers
+{
+    public class NistController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
